1a3b868c1791b05b8dce9efa2ece9d0038a99106
Rony Quispe <oghmand@gmail.com>
2024-07-28 20:47:52 -0500

    move product element

--- src/components/molecules/ProductCard.jsx
+++ src/components/molecules/ProductCard.jsx
@@ -1,24 +1,24 @@
+import { BASE_URL } from "../../constants/env"
+
 function ProductCard({ product }) {
-    return (
-        <div className="card h-100">
-            <img
-                className="card-img-top"
-                th:src="@{/images/{img} (img=${product.getImage()})}"
-                alt=""
-            />
-            <div className="card-body">
-                <p className="card-text" th:text="${product.getName()}"></p>
-            </div>
-            <div className="card-footer">
-                <a
-                    th:href="@{/producthome/{id}  (id=${product.id_product})  }"
-                    className="btn btn-success"
-                >
-                    Ver producto
-                </a>
-            </div>
+  return (
+    <div className="col-lg-3 col-md-6 mb-4" key={product.id_product}>
+      <div className="card h-100">
+        <img className="card-img-top" alt="" />
+        <div className="card-body">
+          <p className="card-text">{product.name}</p>
         </div>
-    )
+        <div className="card-footer">
+          <a
+            href={BASE_URL + "/products/" + product.id_product}
+            className="btn btn-success"
+          >
+            Ver producto
+          </a>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default ProductCard
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -4,6 +4,7 @@ import NavMenu from "../organisms/NavMenu"
 import "../../static/css/heroic-features.css"
 import useFetch from "../../hooks/useFetch"
 import { API_URL, BASE_URL } from "../../constants/env"
+import ProductCard from "../molecules/ProductCard"
 
 const Home = () => {
   const { data, loading, error } = useFetch("products")
@@ -24,22 +25,7 @@ const Home = () => {
             </div>
           )}
           {data?.map((product) => (
-            <div className="col-lg-3 col-md-6 mb-4" key={product.id_product}>
-              <div className="card h-100">
-                <img className="card-img-top" alt="" />
-                <div className="card-body">
-                  <p className="card-text">{product.name}</p>
-                </div>
-                <div className="card-footer">
-                  <a
-                    href={BASE_URL + "/products/" + product.id_product}
-                    className="btn btn-success"
-                  >
-                    Ver producto
-                  </a>
-                </div>
-              </div>
-            </div>
+            <ProductCard key={product.id_product} product={product} />
           ))}
         </div>
       </div>
