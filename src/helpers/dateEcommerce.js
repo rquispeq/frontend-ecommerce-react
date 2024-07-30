588cf6b922cc4822521e4fb658a88cdef429a2be
Rony Quispe <oghmand@gmail.com>
2024-07-29 19:05:04 -0500

    Updates

--- src/helpers/dateEcommerce.js
+++
@@ -1,8 +0,0 @@
-export const parseDate = (date) => {
-  const dateObject = new Date(date)
-  const day = String(dateObject.getDate()).padStart(2, "0")
-  const month = String(dateObject.getMonth() + 1).padStart(2, "0") // Los meses en JavaScript son de 0 a 11
-  const year = dateObject.getFullYear()
-  const formattedDate = `${day}/${month}/${year}`
-  return formattedDate
-}
